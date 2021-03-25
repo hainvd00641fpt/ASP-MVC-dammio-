a8f80f4d5a3f3ceaf9a024898907ef0250835ddc
hainvd00641fpt <hainvd00641@fpt.edu.vn>
2021-03-25 08:16:18 +0700

    p1 -> p5 ASP.NET MVC Dammio.com

---
+++ OneCMVC/Controllers/OneCController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OneCMVC.Controllers
+{
+    public class OneCController : Controller
+    {
+        // GET: OneC
+        public ActionResult  Index()
+        {
+            return View();
+        }
+        // GET: /OneC/ChaoMung/ 
+        public string ChaoMung(string name, int age = 1)
+        {
+            return HttpUtility.HtmlEncode("Xin chao" + name + ". Tuoi cua ban la" + age);
+        }
+
+        public ActionResult Hello(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello" + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        } 
+    }
+}
---
+++ OneCMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OneCMVC.MvcApplication" Language="C#" %>
